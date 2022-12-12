0b6a3dd3c60d66769f61e8b4ee0a047dce767ba8
Chito Wong <chi.to.wong@outlook.com>
2022-12-12 22:16:57 +0800

    Handle BroadcastChannel not defined case and bump version

--- src/service/channel.ts
+++ src/service/channel.ts
@@ -1,13 +1,18 @@
 import { ChannelEventHandler, Events } from '../util/types';
 
 const RMG_RUNTIME_CHANNEL_NAME = 'rmg-runtime-channel';
-
 const eventListeners: Partial<Record<Events, ChannelEventHandler[]>> = {};
-const channel = new BroadcastChannel(RMG_RUNTIME_CHANNEL_NAME);
-channel.onmessage = ev => {
-    const { event, data, frameId } = ev.data;
-    eventListeners[event as Events]?.forEach(cb => cb(data, frameId));
-};
+let channel: BroadcastChannel;
+
+try {
+    channel = new BroadcastChannel(RMG_RUNTIME_CHANNEL_NAME);
+    channel.onmessage = ev => {
+        const { event, data, frameId } = ev.data;
+        eventListeners[event as Events]?.forEach(cb => cb(data, frameId));
+    };
+} catch (e) {
+    console.warn('[rmg-runtime] Failed to initiate broadcast channel. Some features may be unavailable.', e);
+}
 
 const postEvent = (event: Events, data: any) => {
     channel.postMessage({ event, data, frameId: window.frameElement?.id });
